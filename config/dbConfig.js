a586c839af3b0ae23e7dca9ef5c190725266bce4
onurkaraduman <onrkrdmn@gmail.com>
2016-04-02 23:51:18 +0300

    updated

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,12 +12,5 @@ module.exports = {
 		database : "ORGANICFARM",
 		host : "127.0.0.1",
 		dialect : "mysql"
-	},
-	production : {
-		username : "b26e1408f43f58",
-		password : "1b467e06",
-		database : "ad_2ca32c4c5a5a1db",
-		host : "us-cdbr-iron-east-03.cleardb.net",
-		dialect : "mysql"
 	}
 };
